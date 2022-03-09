24e08d60a293389452945f1f491653578ba274c6
Daniel Bowen <djbowen95@gmail.com>
2022-03-09 21:58:52 +0000

    Test for Manager constructor

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,20 @@
+const Manager = require("../lib/Manager");
+
+test("Office Number", () => {
+    const officeNo = 4263;
+    const employee = new Manager("James", 2212, "james@fakeemail.com", officeNo);
+    expect(employee.officeNo).toEqual(officeNo);
+})
+
+test("Get Office Number Function", () => {
+    const officeNo = 4263;
+    const employee = new Manager("James", 2212, "james@fakeemail.com", officeNo);
+    expect(employee.getOfficeNo()).toEqual(officeNo);
+})
+
+
+test("Get Role Function", () => {
+    const role = "Manager";
+    const employee = new Manager();
+    expect(employee.getRole()).toEqual(role);
+})
