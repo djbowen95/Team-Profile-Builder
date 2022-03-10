89642e9a44fe12036169993b9b83f262597a881e
Daniel Bowen <djbowen95@gmail.com>
2022-03-10 17:59:25 +0000

    Clone HTML and CSS files from source to output

--- library/render.js
+++ library/render.js
@@ -1,25 +1,20 @@
 const fs = require("fs");
 const path = require("path");
 
-const source = path.resolve(__dirname, "../source");
+const HTML = path.resolve(__dirname, "../HTML");
 const output = path.resolve(__dirname, "../output");
 const outputHTML = path.join(output, "index.html");
-const outputCSS = path.join(output, "style.css");
 
-fs.writeFileSync(outputHTML, "Something New", "UTF-8");
-fs.writeFileSync(outputCSS, "Another New Thing", "UTF-8");
+function cloneHTML () {
+    const template = fs.readFileSync(path.resolve(HTML, "index.html"), "utf8");
+    fs.writeFileSync(outputHTML, template, "utf8");
+}
 
-/*
-const render = html => {
-    const template = fs.readFileSync(path.resolve(source, "index.html"), "utf8");
-    return replacePlaceholders(template, "team", "Hello");
-  };
+function cloneCSS () {
+    const template = fs.readFileSync(path.resolve(HTML, "style.css"), "utf-8");
+    fs.writeFileSync(path.join(output, "style.css"), template, "utf-8");
+}
 
-const replacePlaceholders = (template, placeholder, value) => {
-    const pattern = new RegExp("{{ " + placeholder + " }}", "gm");
-    console.log(pattern);
-    // return template.replace(pattern, value);
-  };
 
-render();
-*/
+cloneHTML();
+cloneCSS()
